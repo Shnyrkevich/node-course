5ac999f21900adf364d49513cf577705522aca32
Shnyrkevich <mogrein.dark@gmail.com>
2022-10-26 14:48:00 +0300

    feat: basic endpoints configuration, add styles for view templates

--- weather-app/server/src/app.js
+++ weather-app/server/src/app.js
@@ -2,6 +2,7 @@ import Logger from './utils/logger.js';
 import express from 'express';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import hbs from 'hbs';
 import { fetchWeatherInfoByLocation } from './utils/weather.js';
 import { fetchLocation } from './utils/geocode.js'; 
 
@@ -34,40 +35,61 @@ const __dirname = path.dirname(__filename);
 
 // Creating paths to related folder with static/dinamic files
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Set view engine to hbs type instead of html for dynamic rendering
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 app.use(express.static(publicDirectoryPath))
 
 app.get('/', (req, res) => {
 		res.render('index', {
-				title: 'Weather',
+				title: 'Weather app',
 				name: 'Eugene Shnyrkevich'
 		});
-})
+});
 
 app.get('/about', (req, res) => {
 		res.render('about', {
 				title: 'About Me',
 				name: 'Eugene Shnyrkevich'
 		});
-})
+});
 
 app.get('/help', (req, res) => {
 		res.render('help', {
-				helpText: 'This is some helpful text.'
+				title: 'About page',
+				helpText: 'I want to share with you a link',
+				link: 'https://www.udemy.com/course/the-complete-nodejs-developer-course-2/'
 		});
-})
+});
 
 app.get('/weather', (req, res) => {
 		res.send({
+				title: 'Weather by location',
 				forecast: 'It is snowing',
 				location: 'Philadelphia'
 		});
+});
+
+app.get('/help/*', (req, res) => {
+	res.render('error', {
+		title: 'Errored request',
+		code: 404,
+		message: 'Help article not found'
+	})
 })
+
+app.get('*', (req, res) => {
+	res.render('error', {
+		title: 'Errored request',
+		code: 404,
+		message: 'This page doesn\'t exist'
+	})
+});
 
 app.listen(3000, () => {
 	console.log('Server started on port 3000');
