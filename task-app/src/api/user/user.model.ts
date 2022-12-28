0dfb783945d5da80541d6651077fe4a2d1b94da2
Shnyrkevich <mogrein.dark@gmail.com>
2022-12-28 17:07:32 +0300

    fix: change file strucutre

--- task-app/src/api/user/user.model.ts
+++ task-app/src/api/user/user.model.ts
@@ -3,4 +3,7 @@ import UserSchema, { IUser } from './user.schema';
 
 const UserModel = model<IUser>('User', UserSchema, 'users');
 
+console.log('ujif lehasdjansnmdlak!!!')
+// Сообщение дибила
+
 export default UserModel;
