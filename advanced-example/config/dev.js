3ce26165b4ea107409017d25c91037d5010c1395
Shnyrkevich <mogrein.dark@gmail.com>
2023-09-15 15:30:45 +0300

    fix: project setup

--- advanced-example/config/dev.js
+++ advanced-example/config/dev.js
@@ -1,9 +1,7 @@
 module.exports = {
-  googleClientID:
-    "536826033352-p41an3vhnafud5pt2e61k2cukteijpjr.apps.googleusercontent.com",
-  googleClientSecret: "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",
-  mongoURI:
-    "mongodb://localhost:27017/advanced?readPreference=primary&appname=MongoDB%20Compass&ssl=false",
-  cookieKey: "123123123",
-  redisURL: "redis://127.0.0.1:6379",
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  redisURL: process.env.REDIS_URI
 };
